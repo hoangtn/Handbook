a4ced428dbec471999fc552ab9a84d2f1ecdc4df
hoangtn <hoangtannguyenvn@gmail.com>
2015-05-17 18:57:18 +1200

    Update

---
+++ prtUsingCNVrd2.R
@@ -0,0 +1,92 @@
+library("CNVrd2")
+objectCNVrd2 <- new("CNVrd2", windows = windows, chr = "chr5", st = 149500001,
+                    en = 150500000, dirBamFile = dirBamFile,
+                    genes = c(150203161, 150223264), geneNames = "IRGM")
+
+st <- objectCNVrd2@st
+en <- objectCNVrd2@en
+chr <- objectCNVrd2@chr
+gene <- objectCNVrd2@genes
+
+cnOut <- pseudoPRTusingReadDepth(st = st, en = en, chr = chr, gene = gene,
+                                 Nposition = 10,
+                                 dirCoordinate = "../TestSRBreak/TempAll/")
+
+pseudoPRTusingReadDepth <- function(st, en, chr, gene, Nposition, dirCoordinate= "TempAll",
+                                    referenceGenome = "BSgenome.Hsapiens.UCSC.hg19", smallGCdifference = 0.1){
+
+    library(referenceGenome, character.only=TRUE)
+
+    ######Calculate the GC-content of the gene
+    chr <- chr
+    stGene <- gene[1]
+    enGene <- gene[2]
+
+    ####Get the fasta sequence of the chromosome
+    chrSequence <- unmasked(Hsapiens[[chr]])
+
+    geneSequence <- chrSequence[(st):en]
+    gcGene <- 100*sum((alphabetFrequency(geneSequence))[2:3])/sum(alphabetFrequency(geneSequence))
+
+    ####Randomly take Nposition between st and (en - length(gene))
+    sequenceTheSameGC <- c()
+
+    message("GC-content of the gene: ", gcGene)
+    
+    while (length(sequenceTheSameGC) < Nposition){
+
+        seqST <- sample(st:(en - (enGene - stGene + 1)), 1)
+        seqEN <- seqST + (enGene - stGene + 1)
+
+        tempG <- chrSequence[(seqST):seqEN]
+
+        tempGCcontent <- 100*sum((alphabetFrequency(tempG))[2:3])/sum(alphabetFrequency(tempG))
+        if (abs(tempGCcontent - gcGene) <= smallGCdifference){
+            message("Add the position ", seqST, " with the GC-content = ", tempGCcontent)
+            names(tempGCcontent) <- seqST
+            sequenceTheSameGC <- c(sequenceTheSameGC, tempGCcontent)
+       } 
+        
+    }
+
+########################################
+    ####Obtain all positions having similar GC-content
+    finalPos <- as.numeric(names(sequenceTheSameGC))
+
+    finalPosG <- data.frame(finalPos, finalPos + (enGene - stGene + 1))
+
+    finalPosG <- rbind(c(stGene, enGene), finalPosG)
+ ########Read coordinates
+
+    allFile <- dir(path = dirCoordinate, pattern = "coordinate")
+
+  ######Count reads in these positions
+       
+outCount <- lapply(allFile, function(YY) {
+
+    posTemp <- scan(paste(dirCoordinate, YY, sep= ""))
+
+    countPos <- apply(finalPosG, 1, function(x) {
+        return(length(posTemp[(posTemp >= x[1]) & (posTemp <= x[2])]))
+    })
+
+    return(countPos)
+   
+
+})
+
+    outCountMatrix <- do.call(rbind, outCount)
+
+
+    cnOut <- apply(outCountMatrix, 1, function(x) {
+        median(2*x[1]/x[-1], na.rm = TRUE)
+        })
+
+    
+
+    names(cnOut) <- allFile
+
+
+    return(cnOut)
+    
+}
